ea6b82a807ecaf33aa2881c22c7cacd11b404b51
Andre Avila <avila_andre14@hotmail.com>
2022-10-12 22:21:27 +0100

    Added macros

--- LabCode/include/macros.h
+++ LabCode/include/macros.h
@@ -7,6 +7,9 @@
 
 
 #define FLAG 0x7E
-#define A 0x03
-#define C_SET 0x03
-#define C_AU 0x07
+#define A 0x03 //address
+#define C_SET 0x03 //set up
+#define C_AU 0x07 //unnumbered acknowledgement
+#define C_DISC 0x0B //disconnect
+#define C_RR(n) 0x05 | (n>>7) //receiver ready / positive acknowledgement
+#define C_REJ(n) 0x01 | (n>>7) //reject / negative acknowledgment
