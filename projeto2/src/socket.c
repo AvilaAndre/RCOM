20a8a3d94e91cc25e8b0b8ca03160e4795fc2aa2
Andre Avila <up202006767@fe.up.pt>
2022-11-15 14:49:04 +0000

    Reorganized and creted makefile

--- projeto2/src/socket.c
+++ projeto2/src/socket.c
@@ -16,8 +16,8 @@ int connectToSocket(char serverAddress[], int port) {
     /*server address handling*/
     bzero((char *) &server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);    /*32 bit Internet address network byte ordered*/
-    server_addr.sin_port = htons(SERVER_PORT);        /*server TCP port must be network byte ordered */
+    server_addr.sin_addr.s_addr = inet_addr(serverAddress);    /*32 bit Internet address network byte ordered*/
+    server_addr.sin_port = htons(port);        /*server TCP port must be network byte ordered */
 
     /*open a TCP socket*/
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
